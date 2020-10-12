a8a43b749a58fedd8baca868f19f84f773c2be68
div_bhasin <divbest99@gmail.com>
2020-10-12 11:49:14 -0400

    finished 6.2

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,10 +1,31 @@
 from test_framework import generic_test
 
+def convert_base(s, b1, b2):
+    if s == "0" or s == "-0":
+        return s
 
-def convert_base(num_as_string, b1, b2):
-    # TODO - you fill in here.
-    return ''
+    base_ten = 0
+    for i in range(len(s)):
+        if s[i] != "-":
+            digit = s[i]
+            if digit.isalpha():
+                digit = 10 + ord(s[i]) - ord("A")
 
+            base_ten = base_ten * b1 + int(digit)
+
+    res = []
+    while base_ten != 0:
+        digit = base_ten % b2
+        if digit >= 10:
+            digit = chr(ord("A") + (digit % 10))
+
+        res.append("{}".format(digit))
+        base_ten //= b2
+
+    if s[0] == "-":
+        res.append("-")
+
+    return "".join(reversed(res))
 
 if __name__ == '__main__':
     exit(
