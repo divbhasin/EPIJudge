b837c16ff7e2112058f41f6141b252655aac8b2b
div_bhasin <divbest99@gmail.com>
2020-12-13 18:21:43 -0500

    finished EPI 6.7

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -1,10 +1,30 @@
 from test_framework import generic_test, test_utils
 
+mappings = {"2": ['A', 'B', 'C'], \
+        "3": ['D', 'E', 'F'], \
+        "4": ['G', 'H', 'I'], \
+        "5": ['J', 'K', 'L'], \
+        "6": ['M', 'N', 'O'], \
+        "7": ['P', 'Q', 'R', 'S'], \
+        "8": ['T', 'U', 'V'], \
+        "9": ['W', 'X', 'Y', 'Z']}
+
+def permute(phone, pos, i, curr):
+    if i == len(phone):
+        pos.append(curr)
+        return
+
+    num = phone[i]
+    if num != '0' and num != '1':
+        for char in mappings[num]:
+            permute(phone, pos, i+1, curr+char)
+    else:
+        permute(phone, pos, i+1, curr+num)
 
 def phone_mnemonic(phone_number):
-    # TODO - you fill in here.
-    return []
-
+    pos = []
+    permute(phone_number, pos, 0, "")
+    return pos 
 
 if __name__ == '__main__':
     exit(
