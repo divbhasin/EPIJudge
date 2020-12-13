01f974497ce89afc6141d2dc30da671a5fe71176
div_bhasin <divbest99@gmail.com>
2020-12-13 11:41:27 -0500

    done 6.5

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,8 +1,25 @@
 from test_framework import generic_test
 
+def isalphanumeric(c):
+    return (ord(c) >= ord('0') and ord(c) <= ord('9')) or \
+            (ord(c) >= ord('a') and ord(c) <= ord('z')) or \
+            (ord(c) >= ord('A') and ord(c) <= ord('Z'))
 
 def is_palindrome(s):
-    # TODO - you fill in here.
+    i, j = 0, len(s) - 1
+    while i <= j:
+        valid_i, valid_j = isalphanumeric(s[i]), isalphanumeric(s[j])
+        if valid_i and valid_j and s[i].lower() != s[j].lower():
+            return False
+        elif valid_i and valid_j:
+            i += 1
+            j -= 1
+
+        if not valid_i:
+            i += 1
+        if not valid_j:
+            j -= 1
+
     return True
 
 
