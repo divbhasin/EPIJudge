314b3e1316664d3af45502d467581a942287f0d8
div_bhasin <divbest99@gmail.com>
2020-12-14 22:03:36 -0500

    finished 6.8

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def look_and_say(n):
-    # TODO - you fill in here.
-    return ''
+    res = "1"
+    for i in range(1, n):
+        start, curr_res = 1, ""
+        while start <= len(res):
+            count = 1
+            while start > 0 and start < len(res) and res[start] == res[start - 1]:
+                count += 1
+                start += 1
+
+            curr_res += "{}{}".format(count, res[start - 1])
+            start += 1
+
+        res = curr_res
+
+    return res 
 
 
 if __name__ == '__main__':
