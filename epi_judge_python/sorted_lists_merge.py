66169521b9f5b94b02d8dc5f1d77215bc229589e
div_bhasin <divbest99@gmail.com>
2020-12-17 09:05:48 -0500

    finished EPI 7.1 (remember to revisit)

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -1,10 +1,22 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def merge_two_sorted_lists(L1, L2):
-    # TODO - you fill in here.
-    return None
+    curr1, curr2, dum = L1, L2, ListNode()
+    tail = dum
 
+    while curr1 and curr2:
+        if curr1.data < curr2.data:
+            tail.next = curr1
+            curr1 = curr1.next
+        else:
+            tail.next = curr2
+            curr2 = curr2.next
+        tail = tail.next
+
+    tail.next = curr1 if curr2 is None else curr2
+    return dum.next
 
 if __name__ == '__main__':
     exit(
