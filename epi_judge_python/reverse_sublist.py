ee181da9058040409c0e948f9feda2faed2d431e
div_bhasin <divbest99@gmail.com>
2020-12-19 13:56:16 -0500

    finished EPI 7.2 - revisit

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,10 +1,33 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def reverse_sublist(L, start, finish):
-    # TODO - you fill in here.
-    return None
+    dummy = ListNode()
+    dummy.next = L
 
+    pos, prev, curr = 0, None, dummy 
+    while pos < start:
+        prev = curr
+        curr = curr.next
+        pos += 1
+
+    prevStart, startN = prev, curr
+
+    while pos < finish + 1:
+        n = curr.next
+        curr.next = prev
+        prev = curr
+        curr = n
+        pos += 1
+
+    if prevStart and prevStart != prev:
+        prevStart.next = prev
+
+    if startN and startN != curr:
+        startN.next = curr
+
+    return dummy.next 
 
 if __name__ == '__main__':
     exit(
