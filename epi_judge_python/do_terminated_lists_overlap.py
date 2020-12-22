e485e4f63d30287259f5d0d4ec8bf653727886aa
div_bhasin <divbest99@gmail.com>
2020-12-21 19:13:32 -0500

    finished 7.4 (revisit this)

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -6,8 +6,41 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def overlapping_no_cycle_lists(l0, l1):
-    # TODO - you fill in here.
-    return None
+    def length(head):
+        l, curr = 0, head
+        while curr:
+            curr = curr.next
+            l += 1
+
+        return l
+
+    curr0 = l0
+    while curr0 and curr0.next:
+        curr0 = curr0.next
+
+    curr1 = l1
+    while curr1 and curr1.next:
+        curr1 = curr1.next
+
+    if curr1 != curr0 or (curr1 == curr0 and curr1 is None):
+        return None
+
+    len0 = length(l0)
+    len1 = length(l1)
+
+    if len1 > len0:
+        l0, l1 = l1, l0
+
+    curr0 = l0
+    for _ in range(abs(len0 - len1)):
+        curr0 = curr0.next
+
+    curr1 = l1
+    while curr1 != curr0:
+        curr0 = curr0.next
+        curr1 = curr1.next
+
+    return curr0
 
 
 @enable_executor_hook
