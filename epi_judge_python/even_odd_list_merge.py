0669e8d2efd75c946c227b12696dc6414b6a3fd3
div_bhasin <divbest99@gmail.com>
2020-12-22 13:28:19 -0500

    done EPI 7.10

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -1,9 +1,27 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def even_odd_merge(L):
-    # TODO - you fill in here.
-    return None
+    even, odd = ListNode(), ListNode()
+    oddHead, evenHead = odd, even
+
+    curr, pos = L, 0
+
+    while curr:
+        if pos % 2 == 0:
+            even.next = curr
+            even = even.next
+        else:
+            odd.next = curr 
+            odd = odd.next
+
+        curr = curr.next
+        pos += 1
+
+    odd.next = None
+    even.next = oddHead.next
+    return evenHead.next
 
 
 if __name__ == '__main__':
