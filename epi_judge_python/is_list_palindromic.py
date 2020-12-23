9a6f836f1f98eaffb51c7ea117f152f857430cd0
div_bhasin <divbest99@gmail.com>
2020-12-23 10:04:32 -0500

    finished EPI 7.11

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -1,8 +1,38 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def is_linked_list_a_palindrome(L):
-    # TODO - you fill in here.
+    def reverse(head):
+        h = ListNode(0, head)
+        curr = h.next
+
+        while curr and curr.next:
+            t = curr.next
+
+            curr.next = t.next
+            t.next = h.next
+            h.next = t
+
+        return h.next
+
+    dummy = ListNode(0, L)
+    slow, fast = dummy, dummy 
+    while fast and fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    rev = reverse(slow.next)
+    slow.next = None
+
+    curr = L
+
+    while rev and curr:
+        if rev.data != curr.data:
+            return False
+
+        rev, curr = rev.next, curr.next
+
     return True
 
 
