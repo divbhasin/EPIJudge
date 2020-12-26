5e843a57c7437d1a017560b30b2b6db92c48764f
div_bhasin <divbest99@gmail.com>
2020-12-26 10:04:18 -0500

    finished EPI 8.6

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree):
-    # TODO - you fill in here.
-    return []
+    if tree is None:
+        return []
+
+    q = [[tree, 0]]
+    res = []
+
+    while len(q) > 0:
+        curr, d = q.pop(0)
+        if d == len(res):
+            res.append([curr.data])
+        else:
+            res[d].append(curr.data)
+
+        if curr.left:
+            q.append([curr.left, d+1])
+        if curr.right:
+            q.append([curr.right, d+1])
+
+    return res 
 
 
 if __name__ == '__main__':
