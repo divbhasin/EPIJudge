b42dcb5f7b69d35e03c7423b0b172707f77c1c48
div_bhasin <divbest99@gmail.com>
2020-12-28 10:01:42 -0500

    finished EPI 8.3

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def is_well_formed(s):
-    # TODO - you fill in here.
-    return True
+    stack = []
+    pairs = {'}': '{', ']': '[', ')': '('}
+
+    for c in s:
+        if c == '{' or c == '(' or c == '[':
+            stack.append(c)
+        elif len(stack) == 0 or stack[-1] != pairs[c]:
+            return False
+        else:
+            stack.pop()
+
+    return len(stack) == 0 
 
 
 if __name__ == '__main__':
