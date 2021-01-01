013df1e52ba69cbdf3e128b326c49c15068a8900
div_bhasin <divbest99@gmail.com>
2020-12-31 19:16:22 -0500

    finished EPI 9.4

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,7 +2,18 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree):
-    # TODO - you fill in here.
+    def check_equal(n1, n2):
+        if not n1 and not n2:
+            return True
+        elif (n1 and not n2) or (n2 and not n1):
+            return False
+
+        return n1.data == n2.data and check_equal(n1.left, n2.right) and \
+            check_equal(n1.right, n2.left)
+
+    if tree:
+        return check_equal(tree.left, tree.right)
+
     return True
 
 
