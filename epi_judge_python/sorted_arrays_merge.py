e4ef0ef9a2e9276bd4213f8ad9ff6d57836a3ab6
div_bhasin <divbest99@gmail.com>
2021-01-04 16:48:40 -0500

    naive impl of 10.1

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,10 +1,20 @@
 from test_framework import generic_test
+import heapq
 
 
 def merge_sorted_arrays(sorted_arrays):
-    # TODO - you fill in here.
-    return []
+    heap = []
 
+    for arr in sorted_arrays:
+        for elem in arr:
+            heapq.heappush(heap, elem)
+
+    final_arr = []
+    while heap:
+        min_elem = heapq.heappop(heap)
+        final_arr.append(min_elem)
+
+    return final_arr
 
 if __name__ == '__main__':
     exit(
