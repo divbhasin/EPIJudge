ec1062994101b87ec824272fa0365123c271b5bc
div_bhasin <divbest99@gmail.com>
2021-01-06 11:30:57 -0500

    finished EPI 11.4

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def square_root(k):
-    # TODO - you fill in here.
-    return 0
+    lo, hi = 0, k
+    found = 0
+
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+        if mid ** 2 == k:
+            return mid
+        elif mid ** 2 < k:
+            found = mid
+            lo = mid + 1
+        else:
+            hi = mid - 1
+
+    return found 
 
 
 if __name__ == '__main__':
