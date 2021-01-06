7666373ba64701e101a1af9e42623dd1fc6c6003
div_bhasin <divbest99@gmail.com>
2021-01-05 22:15:40 -0500

    simple 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,9 +1,13 @@
 from test_framework import generic_test
+import bisect
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    i = bisect.bisect_left(A, k)
+    if i >= 0 and i < len(A) and A[i] == k:
+        return i
+
+    return -1
 
 
 if __name__ == '__main__':
