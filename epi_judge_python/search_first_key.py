5de821fdb28d3b2f3016f4deb19d1d225fc822d1
div_bhasin <divbest99@gmail.com>
2021-01-05 22:22:05 -0500

    more elaborate 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -3,9 +3,18 @@ import bisect
 
 
 def search_first_of_k(A, k):
-    i = bisect.bisect_left(A, k)
-    if i >= 0 and i < len(A) and A[i] == k:
-        return i
+    lo, hi = 0, len(A) - 1
+
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+        if A[mid] > k:
+            hi = mid - 1
+        elif A[mid] < k:
+            lo = mid + 1
+        elif mid > 0 and A[mid] == k and A[mid - 1] == k:
+            lo, hi = mid - 1, mid - 1
+        else:
+            return mid
 
     return -1
 
