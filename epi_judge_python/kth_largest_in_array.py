3ba55102a82ba85ce5dca763244cece02d8bf0ad
div_bhasin <divbest99@gmail.com>
2021-01-06 22:57:52 -0500

    revisit 11.8

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -1,13 +1,36 @@
 from test_framework import generic_test
+import random
 
 
 # The numbering starts from one, i.e., if A = [3, 1, -1, 2]
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k, A):
-    # TODO - you fill in here.
-    return 0
+    lo, hi = 0, len(A) - 1
+    while lo <= hi:
+        mid = random.randint(lo, hi)
+        pivot = partition(A, lo, hi, mid)
+        if pivot + 1 == k:
+            return A[pivot]
+        elif pivot + 1 > k:
+            hi = pivot - 1
+        else:
+            lo = pivot + 1
 
+    return -1 
+
+def partition(A, lo, hi, mid):
+    pivot_val = A[mid]
+    new_pivot = lo
+    A[hi], A[mid] = A[mid], A[hi]
+
+    for i in range(lo, hi):
+        if A[i] > pivot_val:
+            A[i], A[new_pivot] = A[new_pivot], A[i]
+            new_pivot += 1
+
+    A[new_pivot], A[hi] = A[hi], A[new_pivot]
+    return new_pivot
 
 if __name__ == '__main__':
     exit(
