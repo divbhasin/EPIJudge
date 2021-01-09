f4d19c7ad05853fc074f66eb7f578d204a22c99d
div_bhasin <divbest99@gmail.com>
2021-01-09 11:52:21 -0500

    finished EPI 12.2

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,8 +1,14 @@
 from test_framework import generic_test
+from collections import Counter
 
 
 def is_letter_constructible_from_magazine(letter_text, magazine_text):
-    # TODO - you fill in here.
+    msg_letters = Counter(magazine_text)
+    for c in letter_text:
+        if c not in msg_letters or msg_letters[c] == 0:
+            return False
+        msg_letters[c] -= 1
+
     return True
 
 
