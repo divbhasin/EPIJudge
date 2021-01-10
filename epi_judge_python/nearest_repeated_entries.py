65ef0b5291fcc484d235f66f808ab3fda89dcd2b
div_bhasin <divbest99@gmail.com>
2021-01-10 16:49:46 -0500

    done EPI 12.5

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -2,8 +2,15 @@ from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph):
-    # TODO - you fill in here.
-    return 0
+    words, lowest_dist = {}, float('inf')
+
+    for i in range(len(paragraph)):
+        w = paragraph[i]
+        if w in words:
+            lowest_dist = min(lowest_dist, i - words[w])
+        words[w] = i
+
+    return lowest_dist if lowest_dist < float('inf') else -1
 
 
 if __name__ == '__main__':
