c6853bc107d31703fbd52ab98b04b128b6e673ca
div_bhasin <divbest99@gmail.com>
2021-01-10 18:45:50 -0500

    done EPI 12.1

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
+from collections import Counter
 
 
 def can_form_palindrome(s):
-    # TODO - you fill in here.
-    return True
+    counts = Counter(s)
+    odd = 0
+
+    for c in counts:
+        if counts[c] % 2 != 0:
+            odd += 1
+
+    return odd == 0 or odd == 1 
 
 
 if __name__ == '__main__':
