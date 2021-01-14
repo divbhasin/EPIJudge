74adf46c4959eb00d1af9f7e43bc0e0a586d2f84
div_bhasin <divbest99@gmail.com>
2021-01-13 22:28:40 -0500

    finished EPI 13.1

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    res = []
+    i, j = 0, 0
+
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            res.append(A[i])
+            c = A[i]
+
+            while i < len(A) and j < len(B) and A[i] == B[j] and c == A[i]:
+                i += 1
+                j += 1
+        elif A[i] < B[j]:
+            i += 1
+        else:
+            j += 1
+
+    return res
 
 
 if __name__ == '__main__':
