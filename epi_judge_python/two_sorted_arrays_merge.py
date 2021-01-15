9d20e35a41a0e717bcee6fd797a2ecde3f616d35
div_bhasin <divbest99@gmail.com>
2021-01-14 19:12:27 -0500

    done EPI 13.2

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def merge_two_sorted_arrays(A, m, B, n):
-    # TODO - you fill in here.
-    return
+    i, j, k = m - 1, n - 1, m + n - 1
+    while i >= 0 and j >= 0:
+        if A[i] <= B[j]:
+            A[k] = B[j]
+            j -= 1
+        else:
+            A[k] = A[i]
+            i -= 1
+
+        k -= 1
+
+    while i >= 0:
+        A[k] = A[i]
+        i -= 1
+        k -= 1
+
+    while j >= 0:
+        A[k] = B[j]
+        j -= 1
+        k -= 1
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
